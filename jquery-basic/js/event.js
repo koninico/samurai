d9f79d69de7c917ef5798201d2356c1a518acad7
koninico <125730139+koninico@users.noreply.github.com>
2023-03-25 10:19:38 +0900

    フルーツ２問目解答を横並びにしました。

---
+++ jquery-basic/js/event.js
@@ -0,0 +1,10 @@
+$(function(){
+ $(document).on('click keydown',(e) => {
+  if(e.type === 'click'){
+    $('div').text('クリックされました！');
+  }
+  if(e.type === 'keydown'){
+    $('div').text('キーが押されました！');
+  }
+ });
+});
