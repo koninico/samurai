6ed698bf654bc171d87d1ac19e3f229ecccfa732
koni <konitomo098@gmail.com>
2023-04-08 16:25:41 +0900

    HTML.CSS教材

--- SOCIALTECH-jQuery/script.js
+++ SOCIALTECH-jQuery/script.js
@@ -7,7 +7,7 @@ $(function(){
   });
 
   $('.button-more').on('mouseout',function(){
-    $(this).animate({
+    $(this).animate({  
       opacity: 1.0,
       marginLeft: 0,
     },100);
@@ -31,6 +31,30 @@ $(function(){
   
     // 入力チェックをした結果、エラーがあるかないか判定
     let result = inputCheck();
+
+    //エラー判定とメッセージを取得
+    let error = result.error;
+    let message = result.message;
+
+    //エラーが無かったらフォームを送信する
+    if(error == false) {
+      //Ajaxでformを送信する
+      $.ajax({
+        url: 'https://api.staticforms.xyz/submit',
+        type: 'POST',
+        dataType: 'json',
+        data: $('#form').serialize(),
+        success: function(result){
+          alert('お問い合わせを送信しました。')
+        },
+        error: function(xhr,resp,text){
+          alert('お問い合わせを送信できませんでした。')
+        }
+      })
+    } else {
+      //エラーメッセージを表示する
+      alert(message);
+    }
   });
 
   // フォーカスが外れたとき（blur）にフォームの入力チェックをする
@@ -60,6 +84,86 @@ $(function(){
 
   //お問い合わせフォームの入力チェック
   function inputCheck() {
-    console.log('inputCheck関数の呼び出し');
+    //エラーチェックの結果
+    let result;
+
+    //エラーメッセージのテキスト
+    let message;
+
+    //エラーがなければfalse,エラーがあればtrue
+    //QQエラーの初期値をフォルスとして代入している？？
+    let error = false;
+
+    //お名前のチェック
+    if($('#name').val() ==''){
+      //エラーあり
+      $('#name').css('background-color','#f79999');
+
+      //ここでtrueをエラーに代入してどうするのか？？
+      error = true;
+      message += 'お名前を入力してください。\n';
+
+      } else{
+        //エラーなし
+        $('#name').css('background-color','#fafafa');
+      }
+
+    //ふりがなのチェック
+    if($('#furigana').val() ==''){
+      $('#furigana').css('background-color','#f79999');
+      error = true;
+      message += 'フリガナを入力してください。\n';
+
+      } else{
+        $('#furigana').css('background-color','#fafafa');
+      }
+
+    //お問い合わせのチェック
+    if($('#message').val() ==''){
+      $('#message').css('background-color','#f79999');
+      error = true;
+      message += 'お問い合わせ内容を入力してください。\n';
+      } else{
+        $('#message').css('background-color','#fafafa');
+      }
+
+      if($('#email').val() == ''|| $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1){
+        $('#email').css('background-color','#f79999');
+        error = true;
+        message += 'メールアドレスが未記入、または「@」「.」が含まれていません。\n';
+      }else{
+        $('#email').css('background-color','#fafafa');
+      }
+
+    //電話番号のチェック（未入力はOK、未入力でない場合は-が必要）
+    if($('#tel').val() !='' && $('#tel').val().indexOf('-') == -1){
+        $('#tel').css('background-color','#f79999');
+        error = true;
+        message += '電話番号に「-」が含まれていません。\n';
+      }else{
+        $('#tel').css('background-color','#fafafa');
+      }
+    
+    //個人情報のチェックボックスのチェック
+    if($('#agree').prop('checked') == false){
+      error = true;
+      message += '個人情報の取り扱いについてご同意いただける場合は、チェックボックスにチェックしてください。\n';
+    }
+
+    //エラーの有無で送信ボタンを切り替え
+    if(error == true){
+      $('#submit').attr('src','images/button-submit.png');
+      }else{
+        $('#submit').attr('src','images/button-submit-blue.png');
+      }
+
+    //オブジェクトでエラー判定とメッセージを返す
+    result = {
+      error: error,
+      message: message
+    }
+
+    //戻り値としてエラーがあるかどうかを返す
+    return result;
   }
 });
