c94a30b86d13ea07ebb139b0d9af08daffd56f6e
koni <konitomo098@gmail.com>
2023-04-15 14:03:40 +0900

    途中

--- jQuery/imitation/js/script.js
+++ jQuery/imitation/js/script.js
@@ -1,3 +1,56 @@
-$('.home').hover(function(){
-  $(this).fadeTO("4000",0.3);
+$(function(){
+  //aタグにホバーすると半透明
+  $('a').hover(
+    function(){
+      $(this).animate({opacity:0.5,},400);
+    },
+    function(){
+      $(this).animate({opacity:1.0,},400);
+    }
+  );
+
+   //カルーセル
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    fade: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    speed: 1000,
+  })
+
+  //100pxスクロールするとTOPボタンを表示する
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+      //エラーあり
+      $('#back_btn').fadeIn();
+    }else{
+      //エラーなし
+      $('#back_btn').fadeOut();
+    }
   });
+});
+
+  // topに戻るを押したときにスクロールして戻る
+  $('a[href^="#"]').click(function(){
+    const speed = 500;
+    const href = $(this).attr('href');
+    console.log(href);
+    let $target;
+    if(href == '#'){
+      $target = $('html');
+    }else{
+      $target = $(href);
+    }
+    const position = $target.offset().top;
+    $('html,body').animate({
+      'scrollTop':position},
+      speed,'swing');
+      return false;
+  });
+
+const pos = $('.aboutlink').offset({
+  top: 50,
+  left: 600
+});
+console.log(pos);
---
+++ sample/practice/script.js
@@ -0,0 +1,5 @@
+const pos1 = $('h1').offset();
+const pos2 = $('h1').position();
+ 
+console.log( pos1 );
+console.log( pos2 );
