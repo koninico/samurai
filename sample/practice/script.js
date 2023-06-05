4d25b051646a45867098c44196e3478ba631c392
koni <konitomo098@gmail.com>
2023-06-05 20:15:50 +0900

    JavaScriptテストが動かない

--- sample/practice/script.js
+++ sample/practice/script.js
@@ -1,12 +1,8 @@
-var friends = {
-  name: '太郎',
-  age: 20,
-  address: '東京都渋谷区○○○',
-  '母国語': '日本語'
-}
-
-for ( var list in friends){
-  if(friends.hasOwnProperty(list)){
-    if(friends[list]==='日本語')console.log(list);
+function clickTest() {
+  target = document.getElementById("anime_test");
+  if (target.className == null || target.className=="") {
+    target.className = "active";
+  } else {
+    target.className = "";
   }
 }
